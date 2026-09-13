6eb20c9f3ef9dabd780b748aa454726d634fc646
20250824_yuricks7 <yuricks7+mousePC@dekoboko-hack.biz>
2026-09-13 14:43:14 +0900

    added vitest and RTL.

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom";
+
+// テスト
+describe("テストのテスト", () => {
+  it("1 + 1 は 2", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 import env from "vite-plugin-env-compatible";
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     react(),
-    env({ prefix: "VITE", mountedPath: "process.env" }),
+    env({ prefix: "VITE", mountedPath: "process.env" })
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
